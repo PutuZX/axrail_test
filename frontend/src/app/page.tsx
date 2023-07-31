49b33bce24b1de4a109bd94469825eece48a7523
PutuZX <putubaguswidia@gmail.com>
2023-08-01 07:54:16 +0800

    unavailable contact feature

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -100,66 +100,72 @@ export default function Home() {
             alt="axrail logo"
           />
         </div>
-        <div className="space-y-3 md:bg-gray-200 bg-white p-5 rounded-xl text-black overflow-y-auto h-[300px] md:h-auto md:overflow-y-visible">
-          <div className="space-x-3 items-center text-center md:flex hidden text-[20px]">
-            <div className="w-[400px]">
-              <p>Name</p>
+        {contacts.length > 0 ? (
+          <div className="space-y-3 md:bg-gray-200 bg-white p-5 rounded-xl text-black overflow-y-auto h-[300px] md:h-auto md:overflow-y-visible">
+            <div className="space-x-3 items-center text-center md:flex hidden text-[20px]">
+              <div className="w-[400px]">
+                <p>Name</p>
+              </div>
+              <div className="w-[200px]">
+                <p>Number</p>
+              </div>
             </div>
-            <div className="w-[200px]">
-              <p>Number</p>
-            </div>
+            {contacts.map((contact) => (
+              <div
+                key={contact.id}
+                className="flex md:flex-row flex-col md:space-x-3 space-y-6 md:space-y-0 items-center text-center bg-gray-500 md:bg-transparent md:bg-none p-4 md:p-0 rounded-xl md:rounded-0"
+              >
+                <div className="md:w-[400px] w-[280px] bg-white p-3 rounded-xl">
+                  <p>
+                    {contact.name.length > 35
+                      ? `${contact.name.substring(0, 35)}...`
+                      : contact.name}
+                  </p>
+                </div>
+
+                <div className="md:w-[200px] w-[280px] bg-white p-3 rounded-xl">
+                  <p>{contact.phone}</p>
+                </div>
+                <div className="flex md:space-x-3 space-x-6">
+                  <div className="bg-blue-700 p-3 rounded-xl text-white w-[91.98px]">
+                    <button
+                      onClick={() => {
+                        selectContact(contact.id), handleOpen();
+                      }}
+                    >
+                      <div className="flex space-x-2 items-center text-center">
+                        <Edit style={{ fontSize: "15px" }} />
+                        <div>Edit</div>
+                      </div>
+                    </button>
+                  </div>
+                  <div className="bg-red-700 p-3 rounded-xl text-white">
+                    <button
+                      onClick={() => deleteContact(contact.id)}
+                      className="flex space-x-1 items-center"
+                    >
+                      <Delete style={{ fontSize: "15px" }} />
+                      <div>Delete</div>
+                    </button>
+                  </div>
+                </div>
+
+                <EditModal
+                  id={selectedContact}
+                  handleClose={handleClose}
+                  open={selectedContact !== null}
+                  name={name}
+                  phone={phone}
+                  updateContact={updateContact}
+                />
+              </div>
+            ))}
           </div>
-          {contacts.map((contact) => (
-            <div
-              key={contact.id}
-              className="flex md:flex-row flex-col md:space-x-3 space-y-6 md:space-y-0 items-center text-center bg-gray-500 md:bg-transparent md:bg-none p-4 md:p-0 rounded-xl md:rounded-0"
-            >
-              <div className="md:w-[400px] w-[280px] bg-white p-3 rounded-xl">
-                <p>
-                  {contact.name.length > 35
-                    ? `${contact.name.substring(0, 35)}...`
-                    : contact.name}
-                </p>
-              </div>
-
-              <div className="md:w-[200px] w-[280px] bg-white p-3 rounded-xl">
-                <p>{contact.phone}</p>
-              </div>
-              <div className="flex md:space-x-3 space-x-6">
-                <div className="bg-blue-700 p-3 rounded-xl text-white w-[91.98px]">
-                  <button
-                    onClick={() => {
-                      selectContact(contact.id), handleOpen();
-                    }}
-                  >
-                    <div className="flex space-x-2 items-center text-center">
-                      <Edit style={{ fontSize: "15px" }} />
-                      <div>Edit</div>
-                    </div>
-                  </button>
-                </div>
-                <div className="bg-red-700 p-3 rounded-xl text-white">
-                  <button
-                    onClick={() => deleteContact(contact.id)}
-                    className="flex space-x-1 items-center"
-                  >
-                    <Delete style={{ fontSize: "15px" }} />
-                    <div>Delete</div>
-                  </button>
-                </div>
-              </div>
-
-              <EditModal
-                id={selectedContact}
-                handleClose={handleClose}
-                open={selectedContact !== null}
-                name={name}
-                phone={phone}
-                updateContact={updateContact}
-              />
-            </div>
-          ))}
-        </div>
+        ) : (
+          <div className="text-black py-4 text-lg">
+            There is no contact available in this list. Please try to add them, thank you!
+          </div>
+        )}
         <div className="flex items-center justify-center w-full">
           <div className="bg-green-500 items-center py-3 px-10 rounded-xl mt-5 text-center">
             <Link href="/add_contact">Add new contact</Link>
